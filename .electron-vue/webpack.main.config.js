d15dfe70310d83d8259ce12b6867d9d41080b1b8
Davide Bellini <bellini.davide@gmail.com>
2020-10-15 16:13:38 +0200

    Rimosso babel-minify perché deprecato e configurazioni per pubbliazione release

--- .electron-vue/webpack.main.config.js
+++ .electron-vue/webpack.main.config.js
@@ -6,7 +6,7 @@ const path = require('path')
 const { dependencies } = require('../package.json')
 const webpack = require('webpack')
 
-const MinifyPlugin = require("babel-minify-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin');
 
 let mainConfig = {
   entry: {
@@ -62,7 +62,6 @@ if (process.env.NODE_ENV !== 'production') {
  */
 if (process.env.NODE_ENV === 'production') {
   mainConfig.plugins.push(
-    new MinifyPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': '"production"'
     })
